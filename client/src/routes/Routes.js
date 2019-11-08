6f7a2e6b40396374d12fa97fdfd403636293a52e
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-11-08 10:16:28 -0600

    Added route, content, styling for CourseStructure.js

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,30 +1,29 @@
 import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router";
-import { connect } from 'react-redux';
-import { loggedIn } from '../actions/authenticationActions';
-import Login from '../authentication/Login';
-import Register from '../authentication/Register';
-import LandingPage from '../views/landingPage/components/LandingPage';
-import DashboardView from '../views';
+import { connect } from "react-redux";
+import { loggedIn } from "../actions/authenticationActions";
+import Login from "../authentication/Login";
+import Register from "../authentication/Register";
+import LandingPage from "../views/landingPage/components/LandingPage";
+import DashboardView from "../views";
+import CourseStructure from "../views/courseStructure/CourseStructure";
 
 function Routes(props) {
-
   useEffect(() => {
     props.loggedIn(props.history);
-  }, [])
+  }, []);
 
   return (
     <div>
       <Switch>
-        {props.state.authenticationReducer.user.authenticated &&
-          <Route exact path='/dashboard' render={() =>
-            <DashboardView />
-          } />
-        }
-        <Route path='/login' render={() => <Login />} />
-        <Route path='/register' render={() => <Register />} />
+        {props.state.authenticationReducer.user.authenticated && (
+          <Route exact path="/dashboard" render={() => <DashboardView />} />
+        )}
+        <Route path="/login" render={() => <Login />} />
+        <Route path="/register" render={() => <Register />} />
         <Route exact path="/" render={() => <LandingPage />} />
+        <Route path="/course" render={() => <CourseStructure />} />
       </Switch>
     </div>
   );
@@ -36,7 +35,9 @@ const mapStateToProps = state => {
   };
 };
 
-export default withRouter(connect(
-  mapStateToProps,
-  { loggedIn }
-)(Routes));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { loggedIn }
+  )(Routes)
+);
