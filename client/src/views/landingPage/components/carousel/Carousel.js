505974ddc29ca1aa1b0c0274370f1cbefcbdbbfe
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-11-08 16:15:35 -0500

    Responsiveness changes and adding a gallery slider

--- client/src/views/landingPage/components/carousel/Carousel.js
+++ client/src/views/landingPage/components/carousel/Carousel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './carousel.scss';
+import Slider from "react-slick";
 
 import gallery from '../../../../assets/gallery-carousel-placeholder.png';
 
@@ -7,11 +8,39 @@ import gallery from '../../../../assets/gallery-carousel-placeholder.png';
 
 
 function Carousel() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
         <section className="carousel">
             <div className="wrap">
                 <h2>Gallery</h2>
                 <img src={gallery} />
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                    <div>
+                        <h3>5</h3>
+                    </div>
+                    <div>
+                        <h3>6</h3>
+                    </div>
+                </Slider>
             </div>
         </section>
     )
