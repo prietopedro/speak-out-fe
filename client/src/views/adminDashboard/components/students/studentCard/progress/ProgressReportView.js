5f140062e019bd0a12f74e67e0f53fce2f1f8a7d
Leana <leananeparidze.com>
2019-11-21 11:05:17 -0800

    moves overall to main line where average will be calculated also adds edit button

--- client/src/views/adminDashboard/components/students/studentCard/progress/ProgressReportView.js
+++ client/src/views/adminDashboard/components/students/studentCard/progress/ProgressReportView.js
@@ -4,6 +4,9 @@ import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import './progressReportView.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faSave, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Label = styled.label`
 color: #89878a;
@@ -35,7 +38,10 @@ function ProgressReportView({ report }) {
 
   const [reportDate, setReportDate] = useState();
 
-  const [display, setDisplay] = useState('none')
+  const [display, setDisplay] = useState('none');
+
+  //toggle visibility of cancel button
+  const [displayCancelButton, setDisplayCancelButton] = useState('none');
   
   useEffect(() => {
     console.log('VIEW EXAM: ', report)
@@ -46,12 +52,65 @@ function ProgressReportView({ report }) {
 
   }, [])
 
+  function handleCancel(event) {
+    event.preventDefault();
+    setDisabled(true);
+    setEdit(false);
+    setDisplayCancelButton('none');
+    setCancel(!cancel);
+    setArrowVisibility('Hidden')
+  }
+
+  const handleEdit = () => {
+    // if (edit) {
+    //   const birthdate = moment(student.birthdate).toDate();
+    //   const birthdateISO = birthdate.toISOString();
+
+    //   const schoolGradeUpdated = moment(student.birthdate).toDate();
+    //   const schoolGradeUpdatedISO = schoolGradeUpdated.toISOString();
+
+    //   const editStudent  = {
+    //     id: props.studentById.id,
+    //     cpr: student.cpr, 
+    //     registration_date: props.studentById.registration_date, 
+    //     first_name: student.firstName, 
+    //     additional_names: student.additionalNames, 
+    //     gender: student.gender, 
+    //     birthdate: birthdateISO, 
+    //     school_grade_id: student.schoolGradeId, 
+    //     grade_updated: schoolGradeUpdatedISO,
+    //     school_name: student.schoolName, 
+    //     home_telephone: student.homeTelephone, 
+    //     mobile_telephone: student.mobileTelephone, 
+    //     block_code: student.block, 
+    //     road: student.road, 
+    //     building: student.building, 
+    //     flat: student.flat, 
+    //     email: student.email, 
+    //     notes: student.notes, 
+    //     preferred_contact_type_id: student.contactTypeId, 
+    //     location_id: student.locationId
+    //   }
+    //   props.editStudentById(props.studentById.id, editStudent);
+    //   setDisabled(true);
+    //   setEdit(false);
+    //   setDisplayCancelButton('none');
+    //   setArrowVisibility('Hidden')
+    // } else {
+    //   setDisabled(false);
+    //   setEdit(true);
+    //   setDisplayCancelButton('flex');
+    //   setArrowVisibility('Visible')
+    //   props.resetEdited();
+    // }
+  }
+
   function handleChange(event) {
   }  
 
   function handleDisplay(event) {
     if (display === 'none') {
-      setDisplay('grid');
+      setDisplay('block');
     } else {
       setDisplay('none');
     }
@@ -59,14 +118,57 @@ function ProgressReportView({ report }) {
  
   return (
     <div style={{marginBottom: '10px'}}>
-     <div onClick={handleDisplay} style={{display: 'flex', fontSize: '16px', fontWeight: '500', backgroundColor: '#FAFAFA', 
+     <div onClick={handleDisplay} style={{display: 'flex', alignItems: 'center', ontSize: '16px', fontWeight: '500', backgroundColor: '#FAFAFA', 
                   borderBottom: '1px solid #E8E8E8', borderLeft: '1px solid #f9f7f7', borderRight: '1px solid #f9f7f7', borderTop: '1px solid #f9f7f7',
                   padding: '10px 10px 10px 2px', cursor: 'pointer'}}>
+      <div>
+        {/* <Label>Overall</Label> */}
+        <div style={{width: '35px', margin: '0px 10px 0px 2px'}}>
+          <CircularProgressbar
+            value={report.overall * 10}
+            text={`${report.overall * 10}%`}
+            styles={buildStyles({
+              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+              strokeLinecap: 'round',
+          
+              // Text size
+              textSize: '28px',
+              textWeight: '700',
+          
+              // How long animation takes to go from one percentage to another, in seconds
+              pathTransitionDuration: 0.5,
+          
+              // Colors
+              pathColor: '#1f7e8c',
+              textColor: '#89878a',
+              trailColor: '#d6d6d6',
+              backgroundColor: '#3e98c7',
+          })}/>
+        </div>
+      </div>
       <div style={{marginRight: '10px'}}>SS1</div>
       <div style={{marginRight: '10px'}}>{reportDate}</div>
       <div>Victoria Labdon</div>
      </div>
-     <div style={{display: `${display}`, textAlign: 'left', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr',
+
+     <div style={{display: `${display}`}}>
+      <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <div onClick={handleCancel} 
+             style={{marginTop: '5px', 
+             marginRight: '5px', cursor: 'pointer', 
+             color: '#C73642', display: 'flex', 
+             display: `${displayCancelButton}`}}>
+          <FontAwesomeIcon icon={faTimesCircle} size='lg' color='#C73642' style={{marginRight: '8px'}}/> {''}
+        </div>
+        <div onClick={handleEdit} style={{alignSelf: 'flex-end', marginTop: '5px', cursor: 'pointer', color: '#89878A', display: 'flex'}}>
+          <FontAwesomeIcon icon={edit ? faSave : faEdit} size='lg' color='#bfbfbf' style={{marginRight: '8px'}}/> {''}
+          <div>
+            {edit ? 'Save' : 'Edit'}
+          </div>
+        </div>
+      </div>
+
+     <div style={{display: 'grid', textAlign: 'left', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr',
                  gridGap: '10px', marginTop: '15px'}}>
       {/* <div>
         <Label>Date</Label>
@@ -415,7 +517,7 @@ function ProgressReportView({ report }) {
         </div>
       </div>
 
-      <div>
+      {/* <div>
         <Label>Overall</Label>
         <div style={{width: '90px', marginTop: '10px'}}>
           <CircularProgressbar
@@ -438,13 +540,14 @@ function ProgressReportView({ report }) {
               backgroundColor: '#3e98c7',
           })}/>
         </div>
-      </div>
+      </div> */}
 
       <div style={{gridColumn: 'span 6'}}>
         <Label>Notes</Label>
           <Data>
-           <Input 
-            style={{border: `${edit ? '1px solid #dedbdb' : '1px solid transparent'}`}}
+           <textarea 
+            style={{border: `${edit ? '1px solid #dedbdb' : '1px solid transparent'}`, width: '100%', height: '80px', outline: 'none', 
+            borderRadius: '3px'}}
             type="text"
             name="cpr"
             value={report.notes}
@@ -453,6 +556,7 @@ function ProgressReportView({ report }) {
           </Data>
       </div>
 
+      </div>
     </div>
   </div>
   )
